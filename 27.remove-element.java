1be492b8277412269e44f7af6469adc1cbf3af52
Nikhil Nayan <nayannikhil25@gmail.com>
2025-01-09 12:21:45 +0530

    leetcode-day-01

---
+++ 27.remove-element.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=27 lang=java
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int len = nums.length;
+        int i = 0;
+
+        while (i < len) {
+            if (nums[i] == val) {
+                nums[i] = nums[len - 1];
+                len--;
+            } else {
+                i++;
+            }
+        }
+        return len;
+    }
+}
+// @lc code=end

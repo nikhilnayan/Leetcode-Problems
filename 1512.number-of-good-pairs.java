cefb227a0002bdd0a62fe0511134e60fb4aebd9e
Nikhil Nayan <nayannikhil25@gmail.com>
2025-01-15 17:58:46 +0530

    question no 1512 & 83

---
+++ 1512.number-of-good-pairs.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1512 lang=java
+ *
+ * [1512] Number of Good Pairs
+ */
+
+// @lc code=start
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+// @lc code=end
---
+++ 83.remove-duplicates-from-sorted-list.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=83 lang=java
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        if (head == null)
+            return head;
+
+        ListNode prev = head, curr = head.next;
+
+        while (curr != null) {
+            if (prev.val == curr.val) {
+                prev.next = curr.next;
+                curr = curr.next;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
+// @lc code=end

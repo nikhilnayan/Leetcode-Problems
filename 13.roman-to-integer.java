9946d9342259698c6e3cd1aaf2e420fb244d38e4
Nikhil Nayan <nayannikhil25@gmail.com>
2025-01-19 22:03:27 +0530

    question no 13

--- 13.roman-to-integer.java
+++ 13.roman-to-integer.java
@@ -7,8 +7,52 @@
 // @lc code=start
 class Solution {
     public int romanToInt(String s) {
-        
+
+        int len = s.length();
+        int num = 0;
+
+        for (int i = 0; i < len; i++) {
+            switch (s.charAt(i)) {
+                case 'I':
+                    if (i + 1 < len && (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X')) {
+                        num -= 1;
+                    } else {
+                        num += 1;
+                    }
+                    break;
+                case 'V':
+                    num += 5;
+                    break;
+                case 'X':
+                    if (i + 1 < len && (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')) {
+                        num -= 10;
+                    } else {
+                        num += 10;
+                    }
+                    break;
+                case 'L':
+                    num += 50;
+                    break;
+                case 'C':
+                    if (i + 1 < len && (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')) {
+                        num -= 100;
+                    } else {
+                        num += 100;
+                    }
+                    break;
+                case 'D':
+                    num += 500;
+                    break;
+                case 'M':
+                    num += 1000;
+                    break;
+                default:
+
+                    throw new IllegalArgumentException("Invalid Roman numeral character: " + s.charAt(i));
+            }
+        }
+        return num;
+
     }
 }
 // @lc code=end
-

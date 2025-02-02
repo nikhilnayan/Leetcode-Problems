dd57fde6ef6be7de177b07a7195b9317a838446c
Nikhil Nayan <nayannikhil25@gmail.com>
2025-02-02 11:34:42 +0530

    question no 20

---
+++ 20.valid-parentheses.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+
+    }
+}
+// @lc code=end
